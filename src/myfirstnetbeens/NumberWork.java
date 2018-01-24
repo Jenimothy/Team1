1680e65e72c95ca688ca673fd1eed008ab709b3e
ImageFaded <TBWalton@dundee.ac.uk>
2018-01-24 10:22:53 +0000

    Class that contains functions

---
+++ src/myfirstnetbeens/NumberWork.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myfirstnetbeens;
+
+/**
+ *
+ * @author tommywalton
+ */
+public class NumberWork {
+    
+    public int addNumbers(int n1, int n2){
+        return (n1+n2);
+    }
+    
+    public int subtractNumbers(int n1, int n2){
+        return (n1-n2);
+    }
+   
+    public int multiplyNumbers(int n1, int n2){
+        return (n1*n2);
+    }
+    
+    public int divideNumbers(int n1, int n2){
+        return (n1/n2);
+    }
+    
+    
+    
+}
